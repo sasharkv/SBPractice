53fffabbfb65ce9b7ea2387b958d66bd8b957d16
sasha radkova <sasha@xuntos.nl>
2024-03-25 10:40:55 +0100

    Initial commit

---
+++ src/main/java/com/example/SBPractice/SbPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.SBPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SBPractice/SbPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SBPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
